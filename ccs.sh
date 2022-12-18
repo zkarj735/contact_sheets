2b4815613fe857e1059229ea90926fe610f8c83b
Allister Jenks <62422779+zkarj735@users.noreply.github.com>
2022-12-18 21:09:08 +1300

    Create ccs.sh

---
+++ ccs.sh
@@ -0,0 +1,9 @@
+#!bin/zsh
+for f in "$@"
+do
+  pics=$( /bin/ls -1U "$f"/* | wc -l )
+  n=$( echo "scale=4; $pics/144" | /usr/bin/bc )
+  cols=$( echo "scale=0; sqrt($n) * 16" | /usr/bin/bc )
+  thedir="$(/usr/bin/basename "$f")"
+  nice -n 10 /opt/homebrew/bin/magick montage "$f/*.jp*" -tile "$cols"x -geometry 240x240+4+4 -set label "%t" "$f/../$thedir.jpg"
+done
