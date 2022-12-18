0e4b37a3cffcf64a90feeaacbcf86976e3ca6a75
Allister Jenks <62422779+zkarj735@users.noreply.github.com>
2022-12-18 21:10:03 +1300

    Create ccsm.sh

---
+++ ccsm.sh
@@ -0,0 +1,23 @@
+#!/bin/zsh
+for f in "$@"
+do
+  filenames=( $f/*.jpg )
+  thedir="$(/usr/bin/basename "$f")"
+  pics=${#filenames[@]}
+  sheets=$( echo "scale=0; $pics/144+1" | /usr/bin/bc )
+  offset=0
+  for i in {1..$sheets}
+  do
+    if [ $i -lt $sheets ]; then
+      count=144
+      cols=16
+    else;
+      count=$( echo "scale=0; $pics%144" | /usr/bin/bc )
+      temp=$( echo "scale=4; $count/144" | /usr/bin/bc )
+      cols=$( echo "scale=0; sqrt($temp) * 16" | /usr/bin/bc )
+    fi
+    suffix=$(printf "%02d" "$i")
+    nice -n 10 /opt/homebrew/bin/magick montage "${filenames[@]:$offset:$count}" -tile "$cols"x -geometry 240x240+4+4 -set label "%t" "$f/../$thedir-$suffix.jpg"
+    offset=$offset+144
+  done
+done
