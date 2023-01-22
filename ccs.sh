15d36807f27d25b75b4bcec5f9a6614f42098600
Allister Jenks <62422779+zkarj735@users.noreply.github.com>
2023-01-22 13:48:15 +1300

    Fix: obey TIFF rotation flags in ccsm.sh

--- ccs.sh
+++ ccs.sh
@@ -5,5 +5,5 @@ do
   n=$( echo "scale=4; $pics/144" | /usr/bin/bc )
   cols=$( echo "scale=0; sqrt($n) * 16" | /usr/bin/bc )
   thedir="$(/usr/bin/basename "$f")"
-  nice -n 10 /opt/homebrew/bin/magick montage "$f/*.(jpg|jpeg|tif|tiff|png)" -tile "$cols"x -geometry 240x240+4+4 -set label "%t" "$f/../$thedir.jpg"
+  nice -n 10 /opt/homebrew/bin/magick montage -auto-orient "$f/*.(jpg|jpeg|tif|tiff|png)" -tile "$cols"x -geometry 240x240+4+4 -set label "%t" "$f/../$thedir.jpg"
 done
